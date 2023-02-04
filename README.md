c0ecbe1630443b0a3000b09e03982dd8319efb42
FabioDevCode <0677384849@orange.fr>
2023-02-04 20:28:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JanKenPon
+
+Jeux de Pierre-Papier-Ciseaux
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>
+        <span>JAN</span>
+        <span>KEN</span>
+        <span>PON</span>
+    </h1>
+</template>
+
+<style scoped>
+    h1 {
+        overflow: hidden;
+        font-family: 'Unbounded', cursive;
+        display: flex;
+        justify-content: center;
+        background-color: #FAFAFB;
+        box-shadow: 0 0 12px 2px rgba(75, 82, 122, .5);
+        padding: 20px 25px;
+        font-size: 3em;
+        width: max-content;
+        margin: 15px auto;
+        border-radius: 80px;
+    }
+
+    h1 span {
+        display: flex;
+        justify-content: center;
+        padding: 10px 25px;
+        border-radius: 60px;
+    }
+
+    span:nth-child(1) {
+        color: #464D77;
+        background-color: #EF3D61;
+    }
+    span:nth-child(2) {
+        color: white;
+        background-color: #565C82;
+    }
+    span:nth-child(3) {
+        color: #4B527A;
+        background-color: #F6E56C;
+    }
+</style>
